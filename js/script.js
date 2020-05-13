080f6efeedc965815ab6afa9f1185b6ad2a86d36
DOwdev <63557295+DOwdev@users.noreply.github.com>
2020-05-12 21:57:55 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -152,7 +152,7 @@ pokemonRepository.loadList().then(function () {
 $(document).ready(function () {
   $("#myInput").on("keyup", function () {
     var value = $(this).val().toLowerCase();
-    $("#myDIV *").filter(function () {
+    $("#myDiv *").filter(function () {
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
     });
   });
